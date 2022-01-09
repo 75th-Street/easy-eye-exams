e33eb017a02cf4f215372d1e63b67440a03b1bb7
jonathan grimm <jonathan@salad.com>
2022-01-08 21:30:49 -0700

    Added front end basic structure

--- src/ChartForm.tsx
+++ src/ChartForm.tsx
@@ -1,5 +1,10 @@
-
-
 import React  from "react";
 import { useForm } from "react-hook-form";
 
+const ChartForm = () => (
+        <React.Fragment>
+            //child form components go here
+        </React.Fragment>
+    );
+
+export default ChartForm; 
